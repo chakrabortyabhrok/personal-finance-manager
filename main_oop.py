270359528faa94c02cce8e78ea56123cdf648af6
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-03-17 17:43:10 +0530

    .

--- main_oop.py
+++ main_oop.py
@@ -5,6 +5,7 @@ from manager import FinanceManager
 def main():
     manager = FinanceManager()
     
+    WELCOME_MSG = (f"\n- Budget: ₹{manager.get_budget():.2f} | {manager.get_expense_count()} expenses loaded")
     MENU =  """
                         --- MENU ---
         a - Add Expense              v - View All
@@ -14,8 +15,27 @@ def main():
         f - Show by Category         e - Exit
         """
     while True:
+        print(WELCOME_MSG)
         print(MENU)
         choice = input("- Enter your choice: ").strip().lower()
+         #if choice == "a":
+            
+         #elif choice == "d":
+            
+         #elif choice == "b":
+
+         #elif choice == "u":
+
+         #elif choice == "f":
+
+         #elif choice == "v":
+
+         #elif choice == "s":
+
+         #elif choice == "i":
+
+         #elif choice == "x":
+
         if choice == "e":
             print("-- GOODBYE --")
             break
