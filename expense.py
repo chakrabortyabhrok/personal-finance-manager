7bb6a6110c21c1f51b92e79f4a4e5c53c8dd37e7
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-03-17 13:15:39 +0530

    expense class

--- expense.py
+++ expense.py
@@ -1,20 +1,17 @@
 class Expense:
 
-    def __init__(self, id, date, item, amount, category, payment_method, notes):
+    def __init__(self, id, date, item, amount, category, payment_mehtod, notes):
         self.id = int(id)
         self.date = date
         self.item = item
         self.amount = float(amount)
         self.category = category
-        self.payment_method = payment_method
+        self.payment_method = payment_mehtod
         self.notes = notes
 
-    def display_row(self):
-        return (f"{self.id:<3} | {self.date:<10} | {self.item:<25} | ₹{self.amount:>10.2f} | {self.category:<20} | {self.payment_method:<15} | {self.notes}")
-    
     def to_dict(self):
         return {
-            "id": self.id,
+            "id":self.id,
             "date": self.date,
             "item": self.item,
             "amount": self.amount,
@@ -22,3 +19,7 @@ class Expense:
             "payment_method": self.payment_method,
             "notes": self.notes
         }
+    
+    def display_row(self):
+        return (f"{self.id: <3} | {self.date: 10} | {self.item: 25} | {self.amount:>10.2f} | {self.category: <20} | {self.payment_method: <15} | {self.notes}")
+    
