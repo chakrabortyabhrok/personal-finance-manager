d2e6d24c438a666b0f6fcae6fcb0112b2110d9b3
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-03-18 10:32:14 +0530

    - expense class - REDO

--- expense.py
+++ expense.py
@@ -1,18 +1,18 @@
 class Expense:
 
-    def __init__(self, id, date, item, amount, category, payment_mehtod, notes):
+    def __init__(self, id, date, item, amount, category, payment_method, notes):
         self.id = int(id)
         self.date = date
         self.item = item
         self.amount = float(amount)
         self.category = category
-        self.payment_method = payment_mehtod
+        self.payment_method = payment_method
         self.notes = notes
 
     def to_dict(self):
         return {
-            "id":self.id,
-            "date": self.date,
+            "id": self.id,
+            "date" : self.date,
             "item": self.item,
             "amount": self.amount,
             "category": self.category,
@@ -21,10 +21,10 @@ class Expense:
         }
     
     def display_row(self):
-        return (f"{self.id: <3} | {self.date: 10} | {self.item: 25} | {self.amount:>10.2f} | {self.category: <20} | {self.payment_method: <15} | {self.notes}")
-    
-    def get_month_year(self):
-        return self.date[:7]
+        return (f"{self.id:<3} | {self.date: <10} | {self.item: <25} | {self.amount:>10.2f} | {self.category:<20} | {self.payment_method: <15} | {self.notes}")
     
     def __str__(self):
         return self.display_row()
+    
+    def get_month_year(self):
+        return self.date[:7]
